18009dd9238995789b2966c26513617df8c0b84a
Liam <liamkinazo12@gmail.com>
2026-01-20 01:10:56 +0800

    installing vue router to main and index js

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import { vuetify } from './plugins/vuetify'
+import router from './router'
 
 createApp(App)
   .use(vuetify)
+  .use(router)
   .mount('#app')
 
 // import { createVuetify } from 'vuetify'
